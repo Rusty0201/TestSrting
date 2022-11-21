338e865b49bdfcf123cefb13f49f9f7a63fb6b49
Rusty0201 <ashamano263@gmail.com>
2022-11-21 23:40:15 +0530

    Test Push

---
+++ src/org/test/SampleString.java
@@ -0,0 +1,42 @@
+package org.test;
+
+public class SampleString {
+public static void main(String[] args) {
+	
+	
+	String s="Arjun Athavan";
+	
+	int length = s.length();
+	
+	System.out.println(length);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
